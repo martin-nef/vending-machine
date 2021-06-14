70d87404473a597ee5cb572d3e16ef4812b6f6bc
Martin Nefjodov <Martin.Nefjodov@agileautomations.co.uk>
2021-06-14 12:59:13 +0100

    Implement coin validation

--- Core/Services/MoneyService.cs
+++ Core/Services/MoneyService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Core.Dtos;
+using Core.Enums;
 using Core.Interfaces;
 using Core.Models;
 
@@ -10,26 +11,41 @@ namespace Core.Services {
         private readonly ICoinRepository _coinRepository;
         private readonly ICoinReturnRepository _coinReturnRepository;
 
+        private readonly List<Denomination> ValidCoins = new List<Denomination> {
+            Denomination.Nickel,
+            Denomination.Dime,
+            Denomination.Quarter,
+        };
+
         public MoneyService (
             ICoinRepository coinRepository,
-            ICoinReturnRepository coinReturnRepository)
-        {
+            ICoinReturnRepository coinReturnRepository) {
             _coinRepository = coinRepository;
             _coinReturnRepository = coinReturnRepository;
         }
 
-        public double DisplayCurrentAmount()
-        {
-            throw new NotImplementedException();
+        public double DisplayCurrentAmount () {
+            throw new NotImplementedException ();
         }
 
         public void InsertCoins (ICollection<CoinDto> coinDtos) {
             var coins = ConvertDtosToModels (coinDtos);
-            _coinRepository.InsertCoins (coins);
+            var validCoins = GetValidCoins(coins);
+            var invalidCoins = GetInvalidCoins(coins);
+            _coinRepository.InsertCoins (validCoins);
+            _coinReturnRepository.InsertCoins (invalidCoins);
         }
 
         private ICollection<Coin> ConvertDtosToModels (ICollection<CoinDto> coins) {
             return coins.Select (dto => new Coin (dto)).ToList ();
+        }
+
+        private ICollection<Coin> GetValidCoins (ICollection<Coin> coins) {
+            return coins.Where (coin => ValidCoins.Contains (coin.Type)).ToList ();
+        }
+
+        private ICollection<Coin> GetInvalidCoins (ICollection<Coin> coins) {
+            return coins.Where (coin => !ValidCoins.Contains (coin.Type)).ToList ();
         }
     }
 }
